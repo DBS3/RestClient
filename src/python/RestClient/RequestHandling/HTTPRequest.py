5831b0a741c2f9bb1f115391435b1fc77cafac4f
Manuel Giffels <giffels@gmail.com>
2014-12-09 14:10:21 +0100

    Set content-lenght for DELETE requests.

--- src/python/RestClient/RequestHandling/HTTPRequest.py
+++ src/python/RestClient/RequestHandling/HTTPRequest.py
@@ -44,6 +44,10 @@ class HTTPRequest(object):
             ### set content-length header to ensure performant cherrypy reads
             request_headers['Content-Length'] = str(content_length)
 
+        elif method == 'DELETE':
+            ### set content-length header to avoid "empty relply from server error" from cherrypy
+            request_headers['Content-Length'] = 0
+
         self._curl_options[pycurl.HTTPHEADER] = ["%s: %s" % (key, value) for key, value in request_headers.iteritems()]
 
     def __call__(self, curl_object):
